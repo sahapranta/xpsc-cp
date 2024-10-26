0dca9818ea415cce653cf67e236003cf79ea7e72
sahapranta <pranta1204@gmail.com>
2024-10-26 23:26:22 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Phitron XPSC-05
---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
+
+// g++ hello.cpp -std=c++17
+// ./a.out < in.txt
+// ./a.out < in.txt > out.txt
